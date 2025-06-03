a06bcd8beebeffe64f0853f62debc450e6e051eb
AMFarhan21 <AMFarhan2102@gmail.com>
2025-06-03 14:04:53 +0800

    fix params 4

--- src/app/plants/[slug]/page.tsx
+++ src/app/plants/[slug]/page.tsx
@@ -6,12 +6,12 @@ import { SignIn } from '@stackframe/stack'
 import { Metadata } from 'next'
 
 type PageParams = {
-    params: {
+    params: Promise<{
         slug: string
-    }
+    }>
 }
 
-export async function generateMetadata({params}: PageParams): Promise<Metadata> {
+export async function generateMetadata({ params }: PageParams): Promise<Metadata> {
     const { slug } = await params
     const [id] = slug.split("--")
     const plant = await getPlantsById(id)
@@ -22,13 +22,13 @@ export async function generateMetadata({params}: PageParams): Promise<Metadata> 
     }
 }
 
-export default async function Page({params}: PageParams) {
+export default async function Page({ params }: PageParams) {
     const { slug } = await params
     const user = await stackServerApp.getUser()
     const [id] = slug.split("--")
     const plant = await getPlantsById(id)
 
-    if(!user) return <div className='flex justify-center mt-20'><SignIn /></div>
+    if (!user) return <div className='flex justify-center mt-20'><SignIn /></div>
 
     return (
         <div>
