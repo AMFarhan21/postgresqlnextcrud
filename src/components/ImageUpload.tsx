acf1c3dace7365a45b0e82970dac8393ea4c7a1d
AMFarhan21 <AMFarhan2102@gmail.com>
2025-06-03 15:15:42 +0800

    upload image button

--- src/components/ImageUpload.tsx
+++ src/components/ImageUpload.tsx
@@ -2,7 +2,7 @@
 import { UploadButton } from "@/lib/uploadthing";
 import React from "react";
 import { Button } from "./ui/button";
-import { XIcon } from "lucide-react";
+import { Upload, XIcon } from "lucide-react";
 
 interface ImageUploadProps {
     onChange: (url: string) => void;
@@ -15,7 +15,7 @@ export default function ImageUpload({ endpoint, onChange, value }: ImageUploadPr
     if (value) {
         return (
             <div>
-                <img src={value} alt="Upload" className="rounded-md w-full h-full object-cover" />
+                <img src={value} alt="Upload" className="rounded-md w-20 object-cover" />
                 <Button onClick={() => onChange("")} className="absolute top-0 right-0 p-1 bg-red-500 rounded-full shadow-sm">
                     <XIcon className="h-4 w-4 text-white" />
                 </Button>
@@ -25,7 +25,9 @@ export default function ImageUpload({ endpoint, onChange, value }: ImageUploadPr
 
     return (
         <div className="w-25 flex items-center">
-            <UploadButton
+            <Button variant={"outline"} className="text-foreground flex text-center items-center">
+                <Upload /> Upload Image
+                <UploadButton
                 endpoint={endpoint}
                 onClientUploadComplete={(res) => {
                     // Do something with the response
@@ -40,6 +42,7 @@ export default function ImageUpload({ endpoint, onChange, value }: ImageUploadPr
                     alert(`ERROR! ${error.message}`);
                 }}
             />
+            </Button>
         </div>
     );
 }
